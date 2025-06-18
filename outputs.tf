7a50e38b0da8183af20f962e54bc5c68fe6b65bf
Amar Ubhe <amar.ubhe@sap.com>
2025-06-18 11:13:39 +0530

    Ammendment

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@
 #   description = "BTP Subaccount id."
 # }
 
+
+output "idp_subscription_url" {
+  value       = btp_subaccount_subscription.sap-identity-services-onboarding.subscription_url
+  description = "BTP subscription_url"
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "admin_email" {
     description = "BTP admin user email"
     type        = string
+    default     = "amar.ubhe@sap.com"
 }
 
 variable "admin_password" {
@@ -14,14 +15,15 @@ variable "globalaccount" {
   description = "The globalaccount subdomain.(e.g zzzzzztrial-ga)"
 }
 
-variable "custom_idp" {
-  type        = string
-  description = "Defines the custom IDP to be used for the subaccount(e.g zzzzzz.trial-accounts.ondemand.com)"
-  validation {
-    condition     = can(regex("^[a-z-]", var.custom_idp))
-    error_message = "Please enter a valid entry for the custom-idp of the subaccount."
-  }
-}
+# variable "custom_idp" {
+#   type        = string
+#   description = "Defines the custom IDP to be used for the subaccount(e.g zzzzzz.trial-accounts.ondemand.com)"
+#   default     = "azw61poxu.trial-accounts.ondemand.com"
+#   validation {
+#     condition     = can(regex("^[a-z-]", var.custom_idp))
+#     error_message = "Please enter a valid entry for the custom-idp of the subaccount."
+#   }
+# }
 
 variable "custom_idp_origin" {
   type        = string
